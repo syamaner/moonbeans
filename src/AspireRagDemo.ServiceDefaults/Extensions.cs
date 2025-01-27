5b91fb8db5c01c0dd7c0bf118bc2a39c7c40f131
syamaner <syamaner@users.noreply.github.com>
2025-01-27 14:59:39 +0800

    Added AddGrpcClientInstrumentation so that calls to Qdrant are included in the traces

--- src/AspireRagDemo.ServiceDefaults/Extensions.cs
+++ src/AspireRagDemo.ServiceDefaults/Extensions.cs
@@ -81,7 +81,8 @@ public static class Extensions
             {                
                 tracing.AddAspNetCoreInstrumentation()
                     // Uncomment the following line to enable gRPC instrumentation (requires the OpenTelemetry.Instrumentation.GrpcNetClient package)
-                    //.AddGrpcClientInstrumentation()
+                    .AddGrpcClientInstrumentation()
+                    .AddAspNetCoreInstrumentation()
                     .AddHttpClientInstrumentation();
             });
 
